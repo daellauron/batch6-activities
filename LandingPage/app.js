22bf9a597470244b76488b48d729eef1efef3722
daellauron <61275483+daellauron@users.noreply.github.com>
2021-02-10 15:09:35 +0800

    alright

--- LandingPage/app.js
+++ LandingPage/app.js
@@ -4,4 +4,6 @@ tl.to(".text",{y:"0%", duration:1,stagger:0.25});
 tl.to(".slider",{y:"-100%",duration:1, delay: 0.5});
 tl.to(".intro",{y:"-100%", duration:1},"-=1");
 tl.fromTo('nav',{opacity:0}, {opacity:1,  duration:1});
-tl.fromTo(".bigtext",{opacity:0}, {opacity:1,  duration:1},"-=3");
+tl.fromTo(".bigtext",{opacity:0}, {opacity:1,  duration:1},"-=1");
+tl.fromTo(".btn",{opacity:0}, {opacity:1,  duration:1},"-=1");
+tl.fromTo(".slideshow-container",{opacity:0}, {opacity:1,  duration:1},"-=1");
