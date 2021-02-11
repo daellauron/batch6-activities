00beac250a1d6ad6658f619ac1685e79bd766820
daellauron <61275483+daellauron@users.noreply.github.com>
2021-02-12 00:27:43 +0800

    added

--- LandingPage/app.js
+++ LandingPage/app.js
@@ -1,9 +1,15 @@
 const tl= gsap.timeline({defaults: {ease:"power2.out"}});
 
-tl.to(".text",{y:"0%", duration:1,stagger:0.25});
-tl.to(".slider",{y:"-100%",duration:1, delay: 0.5});
+tl.to(".text",{y:"0%", duration:1,stagger:0.50});
+tl.to(".slider",{y:"-100%",duration:1, delay: 0.25});
 tl.to(".intro",{y:"-100%", duration:1},"-=1");
 tl.fromTo('nav',{opacity:0}, {opacity:1,  duration:1});
 tl.fromTo(".bigtext",{opacity:0}, {opacity:1,  duration:1},"-=1");
 tl.fromTo(".btn",{opacity:0}, {opacity:1,  duration:1},"-=1");
 tl.fromTo(".slideshow-container",{opacity:0}, {opacity:1,  duration:1},"-=1");
+tl.fromTo(".video",{opacity:0}, {opacity:1,  duration:1},"-=1");
+tl.fromTo(".dlbutton1",{opacity:0}, {opacity:1,  duration:1},"-=1");
+tl.fromTo(".dlbutton2",{opacity:0}, {opacity:1,  duration:1},"-=1");
+tl.fromTo(".dlbutton3",{opacity:0}, {opacity:1,  duration:1},"-=1");
+tl.fromTo(".dlbutton4",{opacity:0}, {opacity:1,  duration:1},"-=1");
+tl.fromTo(".arrow",{opacity:0}, {opacity:1,  duration:1},"-=0.01");
